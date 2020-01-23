7d7f40ab7696dd04ba9209dc90c073516e35abff
Stuart Gough <stuartgough7@gmail.com>
2020-01-23 22:56:51 +0000

    Basic 10 by 10 Hex Grid spawned (incorrect positions)

---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject HexPrefab;
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        for (int row = 0; row < 10; row++)
+        {
+            for (int col = 0; col < 10; col++)
+            {
+                //Instantiate Hex Object
+                Instantiate(HexPrefab, new Vector3(col, 0, row), Quaternion.identity);
+            }
+        }
+    }
+
+}
