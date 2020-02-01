11ea159875ae19176b49c301b8e0bdc7d93e6678
Stuart Gough <stuartgough7@gmail.com>
2020-02-01 00:25:46 +0000

    Ramdomization to continent spawn

--- Assets/Scripts/HexMap_Continent.cs
+++ Assets/Scripts/HexMap_Continent.cs
@@ -1,17 +1,24 @@
 ﻿
+using UnityEngine;
+
 public class HexMap_Continent : HexMap
 {
     public override void GenerateMap()
     {
         // First make the base map to generate all Hexs (all water)
         base.GenerateMap();
-        ElevateArea(21, 15, 6);
-        ElevateArea(15, 22, 6);
-        ElevateArea(22, 6, 6);
+        int numSplats = 5;//Random.Range(4, 8);
+        for (int i = 0; i < numSplats; i++)
+        {
+            int range = Random.Range(5, 8);
+            int y = Random.Range(range, numRows - range - 1);
+            int x = Random.Range(0, 10) - y / 2 + 20;
+            ElevateArea(x, y, range);
+        }
         UpdateHexVisuals();
     }
 
-    public void ElevateArea(int q, int r, int range, float centerHeight = 0.5f)
+    public void ElevateArea(int q, int r, int range, float centerHeight = 1f)
     {
         Hex centerHex = GetHexAt(q, r);
         Hex[] areaOfHexes = GetHexesWithinRangeOf(centerHex, range);
@@ -20,7 +27,7 @@ public class HexMap_Continent : HexMap
         {
             if (item.Elevation < 0)
                 item.Elevation = 0;
-            item.Elevation += centerHeight * UnityEngine.Mathf.Lerp(1f, 0.1f, Hex.Distance(centerHex, item)) / range;
+            item.Elevation += centerHeight * Mathf.Lerp(1f, 0.1f, Hex.Distance(centerHex, item)) / range;
         }
     }
 }
