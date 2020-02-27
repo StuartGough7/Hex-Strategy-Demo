9b1a423ef7d71973ef90e25370fa4c905c1f00ce
Stuart Gough <stuartgough7@gmail.com>
2020-02-27 22:16:37 +0000

    Camera motion old postion on Start

--- Assets/Scripts/Camera/CameraMotion.cs
+++ Assets/Scripts/Camera/CameraMotion.cs
@@ -4,40 +4,37 @@ using UnityEngine;
 
 public class CameraMotion : MonoBehaviour
 {
-  // Start is called before the first frame update
+  Vector3 positionOld;
+
   void Start()
   {
-
+    positionOld = this.transform.position;
   }
 
-  public Vector3 positionOld;
-  // Update is called once per frame
   void Update()
   {
-    // @TODO: WSAD move
-    // @TODO: mouse click move
-    // @TODO: zoom in and out
     CheckIfCameraMoved();
   }
 
-  public void PanToHex()
-  {
-    // @TODO: pan to tile move
-  }
+  HexComponent[] hexes;
 
   void CheckIfCameraMoved()
   {
     if (positionOld != this.transform.position)
     {
-      // The camera has moved (could be any input)
+      // Something moved the camera.
       positionOld = this.transform.position;
-      // @TODO: HexMap will probably ahve a dictionary of all Hexxes at somepopint replace this then (PERFORMANCE HIT)
-      HexComponent[] hexxes = FindObjectsOfType<HexComponent>();
-      foreach (HexComponent hex in hexxes)
+
+      // TODO: Probably HexMap will have a dictionary of all these later
+      if (hexes == null)
+        hexes = GameObject.FindObjectsOfType<HexComponent>();
+
+      // TODO: Maybe there's a better way to cull what hexes get updated?
+
+      foreach (HexComponent hex in hexes)
       {
         hex.UpdateHexPosition();
       }
     }
   }
-
 }
