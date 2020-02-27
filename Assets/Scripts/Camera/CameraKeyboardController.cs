dae31a4d76487c50a2745f3d27a00a15427acf6a
Stuart Gough <stuartgough7@gmail.com>
2020-02-27 22:12:29 +0000

    Keyboard Controller updated

--- Assets/Scripts/Camera/CameraKeyboardController.cs
+++ Assets/Scripts/Camera/CameraKeyboardController.cs
@@ -2,14 +2,10 @@
 
 public class CameraKeyboardController : MonoBehaviour
 {
-  void Start()
-  {
-
-  }
-  float moveSpeed = 15f;
+  float moveSpeed = 3.5f;
   void Update()
   {
     Vector3 translate = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxis("Vertical"));
-    this.transform.Translate(translate * moveSpeed * Time.deltaTime, Space.World);
+    this.transform.Translate(translate * moveSpeed * Time.deltaTime * (1 + this.transform.position.y / 2), Space.World);
   }
 }
